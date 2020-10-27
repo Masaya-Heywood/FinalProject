ed97dba2c4506f4b7e3c3728ccd2f25cf7f117c3
Taiyo L Baniecki <tlb416@nyu.edu>
2020-10-27 15:00:38 +0900

    Added Bullet shooting + Player movement

---
+++ Assets/Scripts/ShootingManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingManager : MonoBehaviour
+{
+    private GameObject cursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursor = GameObject.Find("Cursor");
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursor.transform.position = pos + new Vector3(0,0,10);
+    }
+}
