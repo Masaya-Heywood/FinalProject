0d92a0c1004f21267e92919f659794c07e7eed01
Masaya Heywood <mkh429@nyu.edu>
2020-10-27 08:35:10 -0400

    Rough conversion of transform movement to physics movement
    look at commented code in PlayerMovement to get a better view on how it will turn out

--- Assets/Scripts/Player Systems/PlayerMovement.cs
+++ Assets/Scripts/Player Systems/PlayerMovement.cs
@@ -25,27 +25,55 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         //Inital Movement -- We can edit this movement to be on pressing the WASD keys instead of mouse button. We wouldn't need a mouseDirection if we do.
-        if (Input.GetMouseButton(1))
+        if (Input.GetKey(KeyCode.A))
         {
-            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            mouseDirection = (mousePosition - transform.position).normalized;
-            //rb2D.AddForce(mouseDirection * actualInitialSpeed);
-            rb2D.velocity = mouseDirection * (actualInitialSpeed * Time.deltaTime); //change this because players can keep spam clicking for more speed than holding?
+            mouseDirection += transform.right;
         }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            mouseDirection += -transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            mouseDirection += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            mouseDirection += -transform.forward;
+        }
+
+        rb2D.velocity = mouseDirection * (constantSpeed * Time.deltaTime);
+
+        //Vector2 inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //inputVector = inputVector.normalized;
+        //rb2D.velocity = (inputVector * constantSpeed) + new Vector2(0, rb2D.velocity.y);
+
+
+
+        //if (Input.GetMouseButton(1))
+        //{
+        //    mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //    mouseDirection = (mousePosition - transform.position).normalized;
+        //    //rb2D.AddForce(mouseDirection * actualInitialSpeed);
+        //    rb2D.velocity = mouseDirection * (actualInitialSpeed * Time.deltaTime); //change this because players can keep spam clicking for more speed than holding?
+        //}
 
 
         // follow up movement
-        float dist = Vector3.Distance(mousePosition, transform.position);
-        if (dist > 2)
-        {
-            Vector3 dir = (mousePosition - transform.position).normalized;
-            rb2D.velocity = dir * (constantSpeed * Time.deltaTime);
-        }
-        else if (dist <= .5)
-        {
-            rb2D.velocity = Vector3.zero;
-        }
+        //float dist = Vector3.Distance(mousePosition, transform.position);
+        //if (dist > 2)
+        //{
+        //    Vector3 dir = (mousePosition - transform.position).normalized;
+        //    rb2D.velocity = dir * (constantSpeed * Time.deltaTime);
+        //}
+        //else if (dist <= .5)
+        //{
+        //    rb2D.velocity = Vector3.zero;
+        //}
 
         //Rotation
 
