ced52c4e8deb284d8473572118b3f4510bf8f5a5
Taiyo L Baniecki <tlb416@nyu.edu>
2020-10-28 23:17:13 +0900

    Set basics

--- Assets/Scripts/Old/ShootingManager.cs
+++ Assets/Scripts/Old/ShootingManager.cs
@@ -4,18 +4,5 @@ using UnityEngine;
 
 public class ShootingManager : MonoBehaviour
 {
-    private GameObject cursor;
-    // Start is called before the first frame update
-    void Start()
-    {
-        cursor = GameObject.Find("Cursor");
-        Cursor.visible = false;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        cursor.transform.position = pos + new Vector3(0,0,10);
-    }
 }
