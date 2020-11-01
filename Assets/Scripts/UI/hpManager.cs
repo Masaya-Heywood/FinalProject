5760aaf8752556935acb4f0d5dcc8f4199c4d6d3
Taiyo L Baniecki <tlb416@nyu.edu>
2020-11-01 15:49:35 +0900

    Added HP system

---
+++ Assets/Scripts/UI/hpManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hpManager : MonoBehaviour
+{
+    private Image hpImage;
+    private float damage = 0;
+    private float counter = 1;
+    private void Start()
+    {
+        hpImage = this.GetComponent<Image>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (damage >= counter) {
+
+            counter += Time.deltaTime;
+
+            if(hpImage.fillAmount >= 0)
+                hpImage.fillAmount -= Time.deltaTime;
+        }
+
+        //testDamageForDebug
+        if (Input.GetKeyDown(KeyCode.O)) {
+            takeDamate(0.2f);
+        }
+    }
+
+    public void takeDamate(float damage) {
+        this.damage = damage;
+        counter = 0;
+    }
+}
