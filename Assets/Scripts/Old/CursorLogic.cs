e9f156ff16a5322d31398dacad9f6d6bd457a7ec
Masaya Heywood <mkh429@nyu.edu>
2020-11-15 21:20:10 -0500

    Cursor Logic Fix
    Fixed the cursor logic to work in the first level

--- Assets/Scripts/Old/CursorLogic.cs
+++ Assets/Scripts/Old/CursorLogic.cs
@@ -12,7 +12,7 @@ public class CursorLogic : MonoBehaviour
     // Changes the cursor directly using a dedicated cursor asset.
     void Start()
     {
-        cursorTexture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Sprites/Cursors/cursor1White.png", typeof(Texture2D));
+        //cursorTexture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Sprites/Cursors/cursor1White.png", typeof(Texture2D));
         selectSpot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2); //calculates the middle of the texture for selection
         Cursor.SetCursor(cursorTexture, selectSpot, cursorMode);
     }
