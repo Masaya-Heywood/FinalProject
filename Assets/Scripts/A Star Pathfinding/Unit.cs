d26fa45f396dd10673611b3e1add3a3e22f96b60
Masaya Heywood <mkh429@nyu.edu>
2020-11-18 10:38:19 -0500

    Added enemy rotation towards forward movement direction

--- Assets/Scripts/A Star Pathfinding/Unit.cs
+++ Assets/Scripts/A Star Pathfinding/Unit.cs
@@ -71,11 +71,9 @@ public class Unit : MonoBehaviour
 				//transform.position = Vector2.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
 
 				pathDirection = (currentWaypoint - (Vector2)transform.position).normalized;
-                if (rb2D != null)
-                {
-                    
-                    rb2D.velocity = pathDirection * (speed * Time.deltaTime);
-                }
+                if(rb2D != null)
+				    rb2D.velocity = pathDirection * (speed * Time.deltaTime);
+					transform.up = rb2D.velocity;
 				yield return null;
 
 			}
