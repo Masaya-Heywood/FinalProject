ffe2a22432f93f24427d56d7a950aa3455617c95
Taiyo L Baniecki <tlb416@nyu.edu>
2020-11-18 21:43:31 +0900

    Added code to CursorLogic.cs which sets the cursor in the first call of Update

--- Assets/Scripts/Old/CursorLogic.cs
+++ Assets/Scripts/Old/CursorLogic.cs
@@ -9,11 +9,24 @@ public class CursorLogic : MonoBehaviour
     public Vector2 selectSpot = Vector2.zero;
     public CursorMode cursorMode = CursorMode.Auto;
 
+    private bool setCursor = false;
+
     // Changes the cursor directly using a dedicated cursor asset.
     void Start()
     {
         //cursorTexture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Sprites/Cursors/cursor1White.png", typeof(Texture2D));
         selectSpot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2); //calculates the middle of the texture for selection
         Cursor.SetCursor(cursorTexture, selectSpot, cursorMode);
+    }
+
+    private void Update()
+    {
+        if (!setCursor)
+        {
+            //set the cursor in the first call of Update
+            selectSpot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2); //calculates the middle of the texture for selection
+            Cursor.SetCursor(cursorTexture, selectSpot, cursorMode);
+            setCursor = true;
+        }
     }
 }
